6e5d9ac0b1d1377b46f225950b0a766393325df0
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-04-08 10:03:27 +0700

    finished

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Employee extends Model
 {
-    //
+    protected $table = 'employees';
+    protected $primaryKey = 'id';
+     protected $fillable = [
+          'name',
+          'email',
+          'address',
+          'phone',
+      ];
+      use HasFactory;
 }
--- database/migrations/2025_04_07_075322_create_employees_table.php
+++ database/migrations/2025_04_07_075322_create_employees_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('address'); 
+            $table->string('phone');
             $table->timestamps();
         });
     }
